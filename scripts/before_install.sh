a22ccc4b54a269c58374e3538f8a833d65f7e510
omkar3720 <omkarshinde2728@gmail.com>
2025-09-14 09:04:25 +0530

    add some

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
-
 # update os & install python3
 sudo apt-get update
 sudo apt-get install -y python3 python3-dev python3-pip python3-venv
